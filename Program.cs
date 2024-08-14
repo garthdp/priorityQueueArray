57b25dfdbb9daa34384acdcf574d2325eb86be8d
gartdp <garthdp4@gmail.com>
2024-08-14 13:17:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace priorityQueueArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue e = new Queue();
+            e.enqueue(10, 2);
+            e.enqueue(20, 3);
+            e.enqueue(30, 3);
+            e.enqueue(45, 4);
+            e.enqueue(10, 4);
+            e.enqueue(20, 5);
+
+            int iPos = e.peek();
+            Console.WriteLine(e.pr[iPos].value);
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace priorityQueueArray
+{
+    internal class Queue
+    {
+        public item[] pr = new item[100000];
+        int size = -1;
+        public void enqueue(int value, int priority)
+        {
+            size++;
+            pr[size] = new item();
+            pr[size].value = value;
+            pr[size].priority = priority;
+        }
+        public int peek()
+        {
+            int HighestPriority = int.MinValue;
+            int iPos = -1;
+            for (int i = 0; i < size; i++)
+            {
+                if(HighestPriority == pr[i].priority && iPos > -1 && pr[iPos].value < pr[i].value)
+                {
+                    HighestPriority = pr[i].priority;
+                    iPos = i;
+                }
+                else if(HighestPriority < pr[i].priority)
+                {
+                    HighestPriority = pr[i].priority;
+                    iPos = i;
+                }
+            }
+            return iPos;
+        }
+        public void dequeue()
+        {
+            int iPos = peek();
+            for (int i = 0; i < size; ++i)
+            {
+                pr[i] = pr[i + 1];
+            }
+            size--;
+        }
+    }
+}
